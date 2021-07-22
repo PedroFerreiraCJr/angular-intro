cb05a2541bfdcb14de5b9056ef183890c80105a9
Pedro Ferreira <pedroferreiracjr@gmail.com>
2021-07-22 20:11:01 -0300

    add ng-bootstrap; aula sobre class e style binding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { CursosModule } from './cursos/cursos.module';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import { CursosModule } from './cursos/cursos.module';
   imports: [
     BrowserModule,
     CursosModule,
-    DataBindingModule
+    DataBindingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
