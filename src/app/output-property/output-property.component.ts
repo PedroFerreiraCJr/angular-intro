b3eb8d74f998f827da790d636912fe9595827b06
Pedro Ferreira <pedroferreiracjr@gmail.com>
2021-08-02 19:42:04 -0300

    aula sobre viewchild, acessando um componente de template a partir de uma variável de template

--- src/app/output-property/output-property.component.ts
+++ src/app/output-property/output-property.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, ElementRef, EventEmitter, Input, Output, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'contador',
@@ -13,6 +13,13 @@ export class OutputPropertyComponent {
   @Input()
   valor: string = '0';
 
+  // aula sobre @ViewChild(), uma forma de acessar diretamente o elemento que está no template
+  //a partir de uma variável de template que foi previamente declarada. Neste caso a variável
+  //é #campoInput, e está declarada em um elemento do tipo input
+  @ViewChild('campoInput')
+  campoValorInput?: ElementRef;
+
+
   // deve ser utilizado esse decorator para expor um evento deste componente
   @Output('mudou')
   mudouEventEmitter = new EventEmitter<{valor: string}>();
@@ -20,10 +27,12 @@ export class OutputPropertyComponent {
   public incrementar(): void {
     this.valor = parseInt(this.valor, 10) + 1 + '';
     this.mudouEventEmitter.emit({valor: this.valor});
+    console.log('com viewchild: ', this.campoValorInput?.nativeElement.value);
   }
 
   public decrementar(): void {
     this.valor = parseInt(this.valor, 10) - 1 + '';
     this.mudouEventEmitter.emit({valor: this.valor});
+    console.log('com viewchild: ', this.campoValorInput?.nativeElement.value);
   }
 }
